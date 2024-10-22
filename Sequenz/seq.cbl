a8d550599e726feb7c0e6bfcbb32e8bd8e9be50e
Goran Mladenovic <59745165+LastCaballero@users.noreply.github.com>
2024-10-22 23:41:10 +0200

    Update seq.cbl

--- Sequenz/seq.cbl
+++ Sequenz/seq.cbl
@@ -5,6 +5,7 @@ DATA DIVISION.
 WORKING-STORAGE SECTION.
 01 	ST	PIC 	9(10) value 1.
 01	EN	PIC	9(10) value 10.
+01	mem	pic	9(10).
 01	args	pic	x(30).
 01	argc pic	99.
 01	COUNTER PIC	9(10).
@@ -17,6 +18,12 @@ move function length( function trim( args ) ) to argc.
 
 if argc <> 0
 	unstring args delimited by space into st en
+end-if.
+
+if st > en
+	move en  to mem
+	move st  to en
+	move mem to st
 end-if.
 
 PERFORM VARYING COUNTER FROM ST BY 1 UNTIL COUNTER > EN
